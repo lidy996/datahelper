42945caa6665a0e40a256e299d04fc10826f2a38
yzqn1 <yzqn@outlook.com>
2017-11-27 02:52:27 +0800

    增加首页

--- src/main/java/com/yzqn/datahelper/web/controller/UserController.java
+++ src/main/java/com/yzqn/datahelper/web/controller/UserController.java
@@ -2,20 +2,20 @@ package com.yzqn.datahelper.web.controller;
 
 import com.yzqn.datahelper.web.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
 
 @Controller
-@RequestMapping("/user")
 public class UserController {
 
     @Resource
     UserService userService;
 
-    @RequestMapping("/getUser")
-    public String init(){
-        System.out.println(userService.getUser().toString());
+    @RequestMapping("")
+    public String init(Model model){
+        model.addAttribute("message",userService.getUser().getPassword());
         return "index";
     }
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,5 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: yzqn
+  Date: 2017/11/27
+  Time: 2:47
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
+<head>
+    <title>Title</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+<h2>
+    ${message}<br>
+    User: yzqn<br>
+    Date: 2017/11/27<br>
+    Time: 2:47<br>
+</h2>
 </body>
 </html>
